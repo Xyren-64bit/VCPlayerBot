f53934f6777e2ea0644774289bae90045f3cbef4
Xyren <96537396+Xyren-64bit@users.noreply.github.com>
2022-11-01 00:41:19 +0800

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ from pyrogram import (
     Client, 
     filters
 )
-from config import *
+from config import Config
 IST = pytz.timezone(Config.TIME_ZONE)
 if Config.DATABASE_URI:
     from utils import db
